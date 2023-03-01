7d610dda937ab4886c16f0fec5cafc585c1f671e
Martin Björklund <mbj4668@gmail.com>
2023-03-01 21:12:23 +0100

    Picked one code style

--- src/csv.erl
+++ src/csv.erl
@@ -41,93 +41,6 @@ read_file(FName, OptsMap) ->
             {error, {file, Error}}
     end.
 
--define(alt, true).
-
--ifdef(alt).
-p_fields([Sep | T], #opts{separator = Sep} = Opts, Line, FsAcc, RsAcc) ->
-    p_fields(T, Opts, Line, [<<"">> | FsAcc], RsAcc);
-p_fields([$\r, $\n], _Opts, _Line, FsAcc, RsAcc) ->
-    return([<<"">> | FsAcc], RsAcc);
-p_fields([$\r, $\n | T], Opts, Line, FsAcc, RsAcc) ->
-    p_fields(T, Opts, Line+1, [], [lists:reverse([<<"">> | FsAcc]) | RsAcc]);
-p_fields([$\n], _Opts, _Line, FsAcc, RsAcc) ->
-    return([<<"">> | FsAcc], RsAcc);
-p_fields([$\n | T], Opts, Line, FsAcc, RsAcc) ->
-    p_fields(T, Opts, Line+1, [], [lists:reverse([<<"">> | FsAcc]) | RsAcc]);
-p_fields([$# | T], #opts{comments = true} = Opts, Line, [], RsAcc) ->
-    p_fields(skip_to_eol(T), Opts, Line+1, [], RsAcc);
-p_fields([$" | T], Opts, Line, FsAcc, RsAcc) ->
-    p_quoted_field(T, [], Opts, Line, Line, FsAcc, RsAcc);
-p_fields([H | T], Opts, Line, FsAcc, RsAcc) ->
-    p_field(T, [H], Opts, Line, FsAcc, RsAcc);
-p_fields([], _Opts, _Line, FsAcc, RsAcc) ->
-    return(FsAcc, RsAcc).
-
-p_field([Sep], Acc, #opts{separator = Sep}, _Line, FsAcc, RsAcc) ->
-    return([<<"">>, brev(Acc) | FsAcc], RsAcc);
-p_field([Sep | T], Acc, #opts{separator = Sep} = Opts, Line, FsAcc, RsAcc) ->
-    p_fields(T, Opts, Line, [brev(Acc) | FsAcc], RsAcc);
-p_field([$\r, $\n | T], Acc, Opts, Line, FsAcc, RsAcc) ->
-    p_fields(T, Opts, Line+1, [],
-             [lists:reverse([brev(Acc) | FsAcc]) | RsAcc]);
-p_field([$\n | T], Acc, Opts, Line, FsAcc, RsAcc) ->
-    p_fields(T, Opts, Line+1, [],
-             [lists:reverse([brev(Acc) | FsAcc]) | RsAcc]);
-p_field([$" | _], _, #opts{strict = true}, Line, _, _) ->
-    {error, {unescaped_quote, Line}};
-p_field([H | T], Acc, Opts, Line, FsAcc, RsAcc) ->
-    p_field(T, [H | Acc], Opts, Line, FsAcc, RsAcc);
-p_field([], Acc, _Opts, _Line, FsAcc, RsAcc) ->
-    return([brev(Acc) | FsAcc], RsAcc).
-
-p_quoted_field([$", $" | T], Acc, Opts, StartL, Line, FsAcc, RsAcc) ->
-    p_quoted_field(T, [$" | Acc], Opts, StartL, Line, FsAcc, RsAcc);
-p_quoted_field([$" | T], Acc, Opts, _StartL, Line, FsAcc, RsAcc) ->
-    #opts{separator = Sep} = Opts,
-    case T of
-        [] ->
-            return([brev(Acc) | FsAcc], RsAcc);
-        [Sep] ->
-            return([<<"">>, brev(Acc) | FsAcc], RsAcc);
-        [Sep | T2] ->
-            p_fields(T2, Opts, Line, [brev(Acc) | FsAcc], RsAcc);
-        [$\r, $\n | T2] ->
-            p_fields(T2, Opts, Line+1, [],
-                     [lists:reverse([brev(Acc) | FsAcc]) | RsAcc]);
-        [$\n | T2] ->
-            p_fields(T2, Opts, Line+1, [],
-                     [lists:reverse([brev(Acc) | FsAcc]) | RsAcc]);
-        [Ch | _] ->
-            {error, {bad_quote_end, Ch, Line}}
-    end;
-p_quoted_field([$\n | T], Acc, Opts, StartL, Line, FsAcc, RsAcc) ->
-    p_quoted_field(T, [$\n | Acc], Opts, StartL, Line+1, FsAcc, RsAcc);
-p_quoted_field([H | T], Acc, Opts, StartL, Line, FsAcc, RsAcc) ->
-    p_quoted_field(T, [H | Acc], Opts, StartL, Line, FsAcc, RsAcc);
-p_quoted_field([], _, _Opts, StartL, _Line, _FsAcc, _RsAcc) ->
-    {error, {runaway_quote, StartL}}.
-
-skip_to_eol([$\r, $\n | T]) ->
-    T;
-skip_to_eol([$\n | T]) ->
-    T;
-skip_to_eol([_ | T]) ->
-    skip_to_eol(T);
-skip_to_eol([]) ->
-    [].
-
--spec format_error(term()) -> string().
-format_error(empty_file) ->
-    "file is empty";
-format_error({unescaped_quote, Line}) ->
-    io_lib:format("~w: unescaped double quote in field", [Line]);
-format_error({bad_quote_end, Ch, Line}) ->
-    io_lib:format("~w: unexpected character after quote ~s", [Line, [Ch]]);
-format_error({runaway_quote, Line}) ->
-    io_lib:format("~w: runaway quote", [Line]);
-format_error({file, Error}) ->
-    file:format_error(Error).
--else.
 p_fields(L, Opts, Line, FsAcc, RsAcc) ->
     #opts{comments = Comments, separator = Sep} = Opts,
     case L of
@@ -207,7 +120,7 @@ skip_to_eol(L) ->
             T;
         [$\n | T] ->
             T;
-        [_, T] ->
+        [_ | T] ->
             skip_to_eol(T);
         [] ->
             []
@@ -228,7 +141,6 @@ format_error(Error) ->
         {file, Error} ->
             file:format_error(Error)
     end.
--endif.
 
 return([], RsAcc) ->
     {ok, lists:reverse(RsAcc)};
